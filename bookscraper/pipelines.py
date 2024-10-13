3a6bcbdab6b2448f62d1b45344be93eda49d90f8
linhung0319 <linhung0319@gmail.com>
2024-10-13 18:00:27 +0800

    Create item and pipeline for BookItem

--- bookscraper/pipelines.py
+++ bookscraper/pipelines.py
@@ -6,8 +6,69 @@
 
 # useful for handling different item types with a single interface
 from itemadapter import ItemAdapter
+from bookscraper.items import BookItem
 
-
-class BookscraperPipeline:
+class BookItemPipeline:
     def process_item(self, item, spider):
+        ## Only for BookItem
+        if not isinstance(item, BookItem):
+            return item
+        
+        adapter = ItemAdapter(item)
+
+        ## Strip all whitespaces from strings
+        field_names = adapter.field_names()
+        for field_name in field_names:
+            value = adapter.get(field_name)
+            if field_name != 'description':
+                adapter[field_name] = value[0].strip()
+            else:
+                adapter[field_name] = value[0]
+
+
+        ## Category & Product Type => switch to lowercase
+        lowercase_keys = ['category', 'product_type']
+        for lowercase_key in lowercase_keys:
+            value = adapter.get(lowercase_key)
+            adapter[lowercase_key] = value.lower()
+        
+        ## Price => convert to float
+        price_keys = ['price', 'price_excl_tax', 'price_incl_tax', 'tax']
+        for price_key in price_keys:
+            value = adapter.get(price_key)
+            value = value.replace('£', '')
+            adapter[price_key] = float(value)
+
+        ## Availability => extract number of books in stock
+        availability_string = adapter.get('availability')
+        split_string_array = availability_string.split('(')
+        if len(split_string_array) < 2:
+            adapter['availability'] = 0
+        else:
+            availability_array = split_string_array[1].split(' ')
+            adapter['availability'] = int(availability_array[0])
+
+        ## Num Reviews => convert to int
+        num_reviews_string = adapter.get('num_reviews')
+        adapter['num_reviews'] = int(num_reviews_string)
+
+        ## Stars => convert to int
+        stars_string = adapter.get('stars')
+        split_stars_array = stars_string.split(' ')
+        stars_text_value = split_stars_array[1].lower()
+        if stars_text_value == 'zero':
+            adapter['stars'] = 0
+        elif stars_text_value == 'one':
+            adapter['stars'] = 1
+        elif stars_text_value == 'two':
+            adapter['stars'] = 2
+        elif stars_text_value == 'three':
+            adapter['stars'] = 3
+        elif stars_text_value == 'four':
+            adapter['stars'] = 4
+        elif stars_text_value == 'five':
+            adapter['stars'] = 5
+        else:
+            adapter['stars'] = -1
+
         return item
--- bookscraper/spiders/bookspider.py
+++ bookscraper/spiders/bookspider.py
@@ -1,4 +1,5 @@
 import scrapy
+from bookscraper.items import BookItem
 
 
 class BookspiderSpider(scrapy.Spider):
@@ -19,11 +20,11 @@ class BookspiderSpider(scrapy.Spider):
                     book_page_url = 'https://books.toscrape.com/catalogue/' + book_page
                 yield response.follow(book_page_url, callback=self.parse_book_page)
 
-            yield {
-                'name': book.css('h3 a::attr(title)').get(),
-                'price': book.css('.product_price .price_color::text').get(),
-                'url': book.css('h3 a::attr(href)').get()
-            }
+            # yield {
+            #     'name': book.css('h3 a::attr(title)').get(),
+            #     'price': book.css('.product_price .price_color::text').get(),
+            #     'url': book.css('h3 a::attr(href)').get()
+            # }
 
         next_page = response.css('li.next a::attr(href)').get()
 
@@ -36,19 +37,21 @@ class BookspiderSpider(scrapy.Spider):
 
     def parse_book_page(self, response):
         table_rows = response.css('table tr')
+        book_item = BookItem()
 
-        yield {
-            'url': response.url,
-            'title': response.css('.product_main h1::text').get(),
-            'product_type': table_rows[1].css('td::text').get(),
-            'price_excl_tax': table_rows[2].css('td::text').get(),
-            'price_incl_tax': table_rows[3].css('td::text').get(),
-            'tax': table_rows[4].css('td::text').get(),
-            'availability': table_rows[5].css('td::text').get(),
-            'num_reviews': table_rows[6].css('td::text').get(),
-            'stars': response.css('p.star-rating::attr(class)').get().split(' ')[-1],
-            'category': response.xpath('//*[@id="default"]/div/div/ul/li[3]/a/text()').get(),
-            'description': response.xpath('//*[@id="content_inner"]/article/p/text()').get(),
-            'price': response.css('p.price_color::text').get(),
-        }
+        book_item['url'] = response.url,
+        book_item['title'] = response.css('.product_main h1::text').get(),
+        book_item['upc'] = table_rows[0].css('td::text').get(),
+        book_item['product_type'] = table_rows[1].css('td::text').get(),
+        book_item['price_excl_tax'] = table_rows[2].css('td::text').get(),
+        book_item['price_incl_tax'] = table_rows[3].css('td::text').get(),
+        book_item['tax'] = table_rows[4].css('td::text').get(),
+        book_item['availability'] = table_rows[5].css('td::text').get(),
+        book_item['num_reviews'] = table_rows[6].css('td::text').get(),
+        book_item['stars'] = response.css('p.star-rating::attr(class)').get(),
+        book_item['category'] = response.xpath('//*[@id="default"]/div/div/ul/li[3]/a/text()').get(),
+        book_item['description'] = response.xpath('//*[@id="content_inner"]/article/p/text()').get(),
+        book_item['price'] = response.css('p.price_color::text').get(),
+
+        yield book_item
 
